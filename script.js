d9303d748f4bff1750eae524848cc41aeb50e04b
Hanna Ostlund <143076946+hannaostlund@users.noreply.github.com>
2023-11-19 16:48:21 +0100

    Update script.js
    Did all Error handling, such as going over the if/else in addExpense and addIncome functions, so that only correct inputs such as numbers would work
    Spent some time on the listAllExpenses function and added the method "map" so it would list all the expenses in the same alert window.

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-// THE ACCOUNT OBJECT SHOULD HAVE THE FOLLWOING PROPERTIES:
-// getSummary: a function that should summarize your total balances. It should show your total
-// income, your total expenses and also calculate your current balances (income - expenses).
-
 // TODO - my plan
 // 1. Put the name property in the object as the datatype string [DONE]
 // 2. write what the menu should say// Create the menu and different choices for the user [DONE]
@@ -13,12 +9,12 @@
 // 8. Add correct prompt to income and expense in the menu [DONE]
 // 9. Add addExpenses - a function that add expense to the expenses array & explain it [DONE]
 // 10. add function addIncome - add income to income array [DONE]
-// 11. add listAllExpenses function that lists all the expenses in the expenses array. 
-//     Also going over info about the "for" statement loop (at mdn web docs) [DONE]
-// 12. ADD a loop to keep my menu continusly open becuase it closes after adding income : 
+// 11. add listAllExpenses function that lists all the expenses in the expenses array. [DONE]
+// 12. ADD a loop to keep my menu continusly open becuase it closes after adding income : [DONE]
 // 13. add getSummary funcion - that summarizes the total balance, it should show total income, 
-//     total expenses and also calculate currant balances [DONE]
-// 14. Add error message for adding wrong type of income !!! check all error functions
+//     total expenses and also calculate currant balances - Going over the forEach loop at mdn and learn about it [DONE]
+// 14. Add error message for adding wrong type of income and expenses! Go over the error handling and change code if needed [DONE]
+// 15. changed the listAllExpenses and added the map method [DONE]
 
 // --- create the account OBJECT ---
 const account = {
@@ -26,54 +22,60 @@ const account = {
     name: "Hanna",
     expenses: [],
     income: [],
-    // creating the function for addExpense, using a method declaration. (its a property function, so its an function inside an object) 
+    // creating the function for addExpense, this is a function whitin the object "account"
     //using the PUSH array method(because it adds to my exsisting array "expenses")
     addExpense: function (description, amount){
-        if (description !== undefined && amount !== undefined) {
+        if (description && !isNaN(amount) && typeof amount === "number" && amount >= 0) {
+            //error handling, using both typeof amount = number and isNaN to handle error messages  
             this.expenses.push({description, amount});
             //"this" line refer to my expenses array property of the current object (account)
             return true; // successful addition, description and amount will be added if they are not undefined 
         } else {
+            alert("Invalid input! Please enter valid expense and amount")
             return false; //invalid input, not defined 
         }
-    }, // ((btw first I forgot to add a coma here and didnt see why it not worked, now i've learnt that in JS object literals porterties are separated by comas))
+    }, 
     // creating the function for income, using the "push" array method, this function works the same as addExpense  
     addIncome: function (amount) {
-        if (amount !== undefined) {
-            this.income.push(amount);
-            return true; // amount is added if it's not undefined
+        //at first I had written the code "if (amount !== undefined)" and as I progressed in the assignment I realized I needed to be more specific
+        //so that the functions checks that the input is a number 
+        if (isNaN(amount)){
+            alert("Invalid input! Please enter a number for income");
+            return false;
+            //I struggled to handle how to make sure a string couldnt be added to income, and finally i added the isNaN (info from MDN web docs)
         } else {
-            return false; // if it's not defined, it's invalid input
+            this.income.push(amount);
+            alert("Income added");
+            return true;
+            //using the push array to add the income array
         }
     },
     listAllExpenses: function () {
-        //created a function as a method within the object
-        for (let i = 0; i < this.expenses.length; i++) {
-            //using the "for" loop, to go through each expense in the expenses array (using info from mdn web docs)
-            // setting up initialization (let i = 0), condition (i < this.expense.length) and incresment (i++)
-            // I did see in the instructions that you suggested the forEach loop, and I think i could have solved this part with that as well? 
-            //(I will try the forEach loop in the getSummary function to try it out)
-            const currentExpense = this.expenses [i];
-            //get the current expense at index i, within the loop
-            alert (`Expense: ${currentExpense.description}, Amount:${currentExpense.amount}`);
-            // Using templete literal to display alert message (not using console.log as the instructions says)
-        }
+        //At first I created a function for listAll with a for loop, but it listed all expenses seperatly, so I had to research and use
+        // the array method "map", using the link provided by Helena under "array method" video  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+        const allExpenses = this.expenses.map(function (currentExpense) {
+            return `Expense: ${currentExpense.description}, Amount: ${currentExpense.amount}`;
+        });
+        alert("All expenses:\n" + allExpenses.join("\n"));
+        //"map" goes over all expenses in the this.expenses array, and then creating a new array allExpenses with the expense string
+        //Using the "join " method to concatenate all elements in the allExpenses array, and as the join method is a function I call it at the end
     },
     getSummary: function () {
         let totalIncome = 0;
         let totalExpenses = 0;
-        // setting up vsriables and value 
+        // setting up variabls and value 
         this.income.forEach(function(income) {
             totalIncome += income;
         });
-        //using the forEach loop to go through each element in the income array, calculating total income
+        // using the forEach loop to go through each element in the income array, calculating the total income
+        // (using information from MDN web docs on how to use forEach loop)
         this.expenses.forEach(function(expense) {
             totalExpenses += expense.amount;
         });
-        // using forEach loop, calculating total expenses from the expenses array
+        // using the forEach loop, calculating the total expenses from the expenses array
         const balance = totalIncome - totalExpenses;
         // calculating the balance by subtracting total expenses from total income 
-        alert(`Total income ${totalIncome}, total expenses: ${totalExpenses}, balance: ${balance}`);
+        alert(`${this.name} \nYour total income are ${totalIncome}, your total expenses are: ${totalExpenses}, your total balances are: ${balance}`);
         // message displayed using template literate 
     }
 };
@@ -82,9 +84,8 @@ const account = {
 function menu() {
     let continueMenu = true;
        while (continueMenu) {
-        // added a loop to keep the menu open as it closed after adding income, I used the "while" loop which I found googleing
-        //I didnt see any instructions on keeping the menu continusly open, so im not sure if Ive missed something
-        //i found it more user friendly to have the menu open so i didnt have to re-start it
+        // added a loop to keep the menu open as it closed after adding income, I used the "while" loop (info from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/while)
+        // "The while statement creates a loop that executes a specified statement as long as the test condition evaluates to true." 
         const choice = parseFloat(
             prompt(
                 "EXPENSE TRACKER\nPlease select from the menu:\n1.) Add income\n2.) Add expense\n3.) List all expenses\n4.) See total balance"
